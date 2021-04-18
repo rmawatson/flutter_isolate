79b36c87981e89edfcfeaa8c5b93dcce304154fa
Nick Fisher <nick.fisher@avinium.com>
2021-04-18 15:08:05 +0800

    add flutter_downloader to example project

--- example/ios/Runner/AppDelegate.m
+++ example/ios/Runner/AppDelegate.m
@@ -1,12 +1,43 @@
 #include "AppDelegate.h"
 #include "GeneratedPluginRegistrant.h"
+#include "FlutterIsolatePlugin.h"
+#import <path_provider/FLTPathProviderPlugin.h>
+#import <flutter_downloader/FlutterDownloaderPlugin.h>
+
+@interface IsolatePluginRegistrant : NSObject
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry;
+@end
+
+@implementation IsolatePluginRegistrant
+
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+    if(![registry hasPlugin:@"FlutterDownloaderPlugin"]) {
+        NSLog(@"FlutterDownloaderPlugin not found.");
+        [FlutterDownloaderPlugin registerWithRegistrar:[registry registrarForPlugin:@"FlutterDownloaderPlugin"]];
+    }
+    [FLTPathProviderPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTPathProviderPlugin"]];
+    [FlutterIsolatePlugin registerWithRegistrar:[registry registrarForPlugin:@"FlutterIsolatePlugin"]];
+}
+
+@end
+
 
 @implementation AppDelegate
 
+void registerPlugins(NSObject<FlutterPluginRegistry>* registry) {
+    if(![registry hasPlugin:@"FlutterDownloaderPlugin"]) {
+        [FlutterDownloaderPlugin registerWithRegistrar:[registry registrarForPlugin:@"FlutterDownloaderPlugin"]];
+    }
+}
+
+
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-  [GeneratedPluginRegistrant registerWithRegistry:self];
-  // Override point for customization after application launch.
+
+    FlutterIsolatePlugin.isolatePluginRegistrantClassName = @"IsolatePluginRegistrant";
+  [IsolatePluginRegistrant registerWithRegistry:self];
+  [FlutterDownloaderPlugin setPluginRegistrantCallback:registerPlugins];
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
